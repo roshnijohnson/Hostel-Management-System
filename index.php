ff1818ada7b1689f47d1760671b8feec919dbb5f
ronileonita <ronileonita61299@gmail.com>
2021-07-21 23:41:53 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,176 @@
+<?php
+session_start();
+include('includes/config.php');
+if(isset($_POST['login']))
+{
+$email=$_POST['email'];
+$password=$_POST['password'];
+$stmt=$mysqli->prepare("SELECT email,password,id FROM userregistration WHERE email=? and password=? ");
+				$stmt->bind_param('ss',$email,$password);
+				$stmt->execute();
+				$stmt -> bind_result($email,$password,$id);
+				$rs=$stmt->fetch();
+				$stmt->close();
+				$_SESSION['id']=$id;
+				$_SESSION['login']=$email;
+				$uip=$_SERVER['REMOTE_ADDR'];
+				$ldate=date('d/m/Y h:i:s', time());
+				if($rs)
+				{
+             $uid=$_SESSION['id'];
+             $uemail=$_SESSION['login'];
+$ip=$_SERVER['REMOTE_ADDR'];
+$geopluginURL='http://www.geoplugin.net/php.gp?ip='.$ip;
+$addrDetailsArr = unserialize(file_get_contents($geopluginURL));
+$city = $addrDetailsArr['geoplugin_city'];
+$country = $addrDetailsArr['geoplugin_countryName'];
+$log="insert into userLog(userId,userEmail,userIp,city,country) values('$uid','$uemail','$ip','$city','$country')";
+$mysqli->query($log);
+if($log)
+{
+header("location:dashboard.php");
+die();
+				}
+}
+				else
+				{
+					echo "<script>alert('Invalid Username/Email or password');</script>";
+				}
+			}
+				?>
+
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">m
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="theme-color" content="#3e454c">
+	<title>Student Hostel Registration</title>
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">>
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+	<link rel="stylesheet" href="css/bootstrap-select.css">
+	<link rel="stylesheet" href="css/fileinput.min.css">
+	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+	<link rel="stylesheet" href="css/style.css">
+<script type="text/javascript" src="js/jquery-1.11.3-jquery.min.js"></script>
+<script type="text/javascript" src="js/validation.min.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+<script type="text/javascript">
+function valid()
+{
+if(document.registration.password.value!= document.registration.cpassword.value)
+{
+alert("Password and Re-Type Password Field do not match  !!");
+document.registration.cpassword.focus();
+return false;
+}
+return true;
+}
+</script>
+</head>
+<body -*
++///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 	<?php include('includes/header.php');?>
+	<div class="ts-main-content">
+		<?php include('includes/sidebar.php');?>
+		<div class="content-wrapper">
+			<div class="container-fluid">
+
+				<div class="row">
+					<div class="col-md-12">
+					
+						<h2 class="page-title" >User Login </h2>
+
+						<div class="row">
+					<div class="col-md-6 col-md-offset-3">
+						<div class="well row pt-2x pb-3x bk-light">
+							<div class="col-md-8 col-md-offset-2">
+							
+								<form action="" class="mt" method="post">
+									<label for="" class="text-uppercase text-sm">Email</label>
+									<input type="text" placeholder="Email" name="email" class="form-control mb">
+									<label for="" class="text-uppercase text-sm">Password</label>
+									<input type="password" placeholder="Password" name="password" class="form-control mb">
+									
+
+									<input type="submit" name="login" class="btn btn-primary btn-block" value="login" >
+								</form>
+							</div>
+						</div>
+						<div class="text-center text-light" style="color:white;">
+							<a href="forgot-password.php" style="color:white;">Forgot password?</a>
+						</div>
+							<div class='tc pa2 white'>
+	<div style="color: white" id="title" class='tc'></div>
+	<div style="color: white" id='author'> </div>
+	<div style="color: white" id='author1'> </div>
+	<div style="color: white" id='author2'> </div>
+	<div style="color: white" id='author3'> </div>
+	<div style="color: white" class="mb3" id='copyright'> </div>
+</div>
+					</div>
+				</div>
+						</div>
+							</div>
+						</div>
+					</div>
+				</div> 	
+			</div>
+		</div>
+
+
+<!-- 	</div> -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap-select.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script src="js/dataTables.bootstrap.min.js"></script>
+	<script src="js/Chart.min.js"></script>
+	<script src="js/fileinput.js"></script>
+	<script src="js/chartData.js"></script>
+	<script src="js/main.js"></script>
+
+
+<script>
+	var parser, xmlDoc;
+	var text = "<footer><text>" +
+		"<title>Welcome to database management system</title>" +
+		"<author>parth shinde</author>" +
+		"<author1>rohan sawant</author1>" +
+		"<author2>roshni johnson</author2>" +
+		"<author3>roni leonita</author3>" +
+		"<copyright>FCRIT,Vashi Copyright @2019</copyright>" +
+		"</text></footer>";
+
+	parser = new DOMParser();
+	xmlDoc = parser.parseFromString(text, "text/xml");
+
+	document.getElementById("title").innerHTML =
+		xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+
+	document.getElementById("author").innerHTML =
+		xmlDoc.getElementsByTagName("author")[0].childNodes[0].nodeValue;
+
+	document.getElementById("author1").innerHTML =
+		xmlDoc.getElementsByTagName("author1")[0].childNodes[0].nodeValue;
+
+	document.getElementById("author2").innerHTML =
+		xmlDoc.getElementsByTagName("author2")[0].childNodes[0].nodeValue;
+
+	document.getElementById("author3").innerHTML =
+		xmlDoc.getElementsByTagName("author3")[0].childNodes[0].nodeValue;
+
+	document.getElementById("copyright").innerHTML =
+		xmlDoc.getElementsByTagName("copyright")[0].childNodes[0].nodeValue;
+</script>
+
+</body>
+
+
+
+</html>
